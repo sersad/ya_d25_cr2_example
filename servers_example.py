da2c5c14652499c55e8e0eb7e1763351653b080e
Sergey Sadovnikov <sergey.sadovnikov@gmail.com>
2026-04-23 17:44:53 +0300

    first commit

---
+++ servers_example.py
@@ -0,0 +1,50 @@
+import io
+import logging
+from contextlib import contextmanager, redirect_stdout
+from json import dumps
+from multiprocessing import Process
+from time import sleep
+
+from flask import Flask
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+
+class Server:
+    def __init__(self, host, port, data):
+        self.__host__ = host
+        self.__port__ = port
+        self.__data__ = data
+
+    @contextmanager
+    def run(self):
+        p = Process(target=self.server)
+        p.start()
+        sleep(1)
+        yield
+        p.kill()
+
+    def server(self):
+        _ = io.StringIO()
+        with redirect_stdout(_):
+            app = Flask(__name__)
+
+            @app.route('/')
+            def index():
+                return dumps(self.__data__)
+
+            app.run(self.__host__, self.__port__)
+
+
+if __name__ == '__main__':
+    data = [
+            {'alpha': [10, 20, 30], 'beta': [5, 15]},
+            {'alpha': [100, 50], 'beta': [75, 25, 50, 10]},
+            {'alpha': [8, 12, 16, 20], 'beta': [14, 18]}
+        ]
+
+    server = Server('127.0.0.1', 8080, data)
+    with server.run():
+        while (row := input('Введите "stop" для завершения работы сервера: ')) != 'stop':
+            ...
